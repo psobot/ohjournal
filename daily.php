93accecd475e732d2695e6aa74b260513dea06a9
Peter Sobot <psobot@gmail.com>
2010-12-09 21:09:23 -0500

    Fixed date error.

--- daily.php
+++ daily.php
@@ -10,7 +10,7 @@
 	date_default_timezone_set("America/Toronto");		//set your timezone here if this is incorrect
 	$yourEmail	=	"psobot+ohjournal@gmail.com";			//the email you want to recieve your journal prompts to
 	$saveEmail	=	"psobot+ohjournalresponse@gmail.com";	//the email you want to save your journal entries at
-	$subject	=	"It's " . date("l, F n") . " - How did your day go?";
+	$subject	=	"It's " . date("l, F j") . " - How did your day go?";
 	$body		=	"Just respond to this email with your entry, and it will be saved automagically.";
 	$headers	=	'From: OhJournal <ohjournal@petersobot.com>' . "\r\n" .
    					'Reply-To: ' . $saveEmail . "\r\n" .
