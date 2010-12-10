f68d75964748ecdf6c6d97d031c0bc5b37b18436
Peter Sobot <psobot@gmail.com>
2010-12-10 00:19:27 -0500

    Removed my email.

--- daily.php
+++ daily.php
@@ -8,11 +8,11 @@
 	 */
 
 	date_default_timezone_set("America/Toronto");		//set your timezone here if this is incorrect
-	$yourEmail	=	"psobot+ohjournal@gmail.com";			//the email you want to recieve your journal prompts to
-	$saveEmail	=	"psobot+ohjournalresponse@gmail.com";	//the email you want to save your journal entries at
+	$yourEmail	=	"youremail+ohjournal@gmail.com";			//the email you want to recieve your journal prompts to
+	$saveEmail	=	"youremail+ohjournalresponse@gmail.com";	//the email you want to save your journal entries at
 	$subject	=	"It's " . date("l, F j") . " - How did your day go?";
 	$body		=	"Just respond to this email with your entry, and it will be saved automagically.";
-	$headers	=	'From: OhJournal <ohjournal@petersobot.com>' . "\r\n" .
+	$headers	=	'From: OhJournal <ohjournal@petersobot.com>' . "\r\n" .	//may need to change this domain to yours if your emails won't send
    					'Reply-To: ' . $saveEmail . "\r\n" .
     				'X-Mailer: PHP/' . phpversion();
 
