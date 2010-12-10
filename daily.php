4fc62af1c4e11b11e2ddaa97119f9c8dc2d6961c
Peter Sobot <psobot@gmail.com>
2010-12-09 20:52:42 -0500

    Progress on mailing script.

--- daily.php
+++ daily.php
@@ -1,3 +1,20 @@
 <?php
-	mail("psobot+ohjournal@gmail.com", "Test SMTP", "Message!");
+	
+	/*
+	 *	OhJournal - open-source replacement for OhLife
+	 *	http://github.com/psobot/ohjournal
+	 *
+	 *	by Peter Sobot <hi@petersobot.com>
+	 */
+
+	date_default_timezone_set("America/Toronto");		//set your timezone here if this is incorrect
+	$yourEmail	=	"psobot+ohjournal@gmail.com";			//the email you want to recieve your journal prompts to
+	$saveEmail	=	"psobot+ohjournalresponse@gmail.com";	//the email you want to save your journal entries at
+	$subject	=	"It's " . date("l, F n") . " - How did your day go?";
+	$body		=	"Just respond to this email with your entry, and it will be saved automagically.";
+	$headers	=	'From: ohjournal@petersobot.com' . "\r\n" .
+   					'Reply-To: ' . $saveEmail . "\r\n" .
+    				'X-Mailer: PHP/' . phpversion();
+
+	mail($yourEmail, $subject, $body, $headers);
 ?>
