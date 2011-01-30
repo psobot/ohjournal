60c8e0c7318f0acd67a893f29e440ffa3d464e2c
Peter Sobot <psobot@gmail.com>
2011-01-29 21:37:55 -0500

    Whoops. Error.

--- ohjournal.php
+++ ohjournal.php
@@ -80,7 +80,7 @@
 		 */
 		public function parseMailFile($raw){
 			$m = preg_match("/^From ".str_replace(".", "\.", Config::$yourEmail).".+([\s\S]+?)^From /", $raw, $matches);
-			var_dump($matches);
+			var_dump($raw, $matches, "/^From ".str_replace(".", "\.", Config::$yourEmail).".+([\s\S]+?)^From /");
 			return trim($matches[1]);
 		}
 
