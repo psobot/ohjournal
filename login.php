527cc444bff3cc479f1588f220ef1ce018a227b1
Peter Sobot <psobot@gmail.com>
2011-02-06 23:46:59 -0500

    Removed placeholder from password field.

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
 	<div id="unlock">
 		<form action="./" method="post">
 			<input type="text" name="password" value="password" id="passwordHolder" />
-			<input type="password" name="password" value="password" id="password" />
+			<input type="password" name="password" value="" id="password" />
 			<input type="submit" name="submit" value="unlock" />
 		</form>
 	</div>
