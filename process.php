e9d44124e444e3fae5743f1a7a14cb60b2bb0377
Peter Sobot <psobot@gmail.com>
2011-02-14 23:34:29 -0500

    Fixing things?

--- process.php
+++ process.php
@@ -2,7 +2,6 @@
 	require_once("ohjournal.php");
 	$raw = file_get_contents("php://stdin");
 	if($raw == "")	$raw = $j->parseMailFile($j->getMail());
-	var_dump($raw);
 	if(trim($raw) == "" || $raw == false)	return false;
 	$data = $j->parseEmail($raw);
 	if($j->submitEntry($data[0], $data[1], $data[2], $data[3])){
