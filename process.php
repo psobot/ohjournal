e60c99c5a6a10a446d2ae0adf74a5b0b5a6eb5b4
Peter Sobot <psobot@gmail.com>
2011-02-14 22:30:03 -0500

    Fixed issues in responseEmail that prevented some email reception. Again. Stupid issue.

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
 	require_once("ohjournal.php");
 	$raw = file_get_contents("php://stdin");
-	if($stdin == "")	$raw = $j->parseMailFile($j->getMail());
+	if($raw == "")	$raw = $j->parseMailFile($j->getMail());
 	if(trim($raw) == "" || $raw == false)	return false;
 	$data = $j->parseEmail($raw);
 	if($j->submitEntry($data[0], $data[1], $data[2], $data[3])){
