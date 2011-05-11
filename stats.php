9d86051b2734882aa9136d20c224463a1b577964
Peter Sobot <psobot@gmail.com>
2011-05-11 13:41:46 -0400

    Stats: Changed word frequency metric to be more useful.

--- stats.php
+++ stats.php
@@ -23,9 +23,11 @@
 				$lengths["Date.UTC(".($dateParts[0]).", ".($dateParts[1]-1).", ".($dateParts[2]).")"] = count(explode(" ", $entry['entry']));
 				foreach(explode(" ", $entry['entry']) as $word){
 					$word = strtolower(preg_replace('/[^a-zA-Z0-9 ]/','',$word));	//remove non-alphanumeric chars
-					$words[$word]++;			//increment histogram
-					$wordCount++;				//increment wordcount
-					$charCount+=strlen($word);	//increment charcount
+					if(strlen($word) >= 5){
+						$words[$word]++;			//increment histogram
+						$wordCount++;				//increment wordcount
+						$charCount+=strlen($word);	//increment charcount
+					}
 				}	
 			}
 		}
